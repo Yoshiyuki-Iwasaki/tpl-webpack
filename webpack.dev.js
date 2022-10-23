597f5ec770a34a1095a3b7f63ab1d86cda38dfbf
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-10-23 22:32:00 +0900

    fix filename.

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,15 +1,15 @@
-const { resolve } = require("path");
-const merge = require("webpack-merge");
-const common = require("./webpack.common.js");
+const { resolve } = require('path');
+const merge = require('webpack-merge');
+const common = require('./webpack.config.js');
 
 module.exports = merge(common, {
-  mode: "development",
-  devtool: "inline-source-map",
+  mode: 'development',
+  devtool: 'inline-source-map',
   devServer: {
     hot: true,
     compress: true,
     writeToDisk: true,
-    contentBase: resolve(__dirname, "dist"),
+    contentBase: resolve(__dirname, 'dist'),
     watchContentBase: true,
     open: true,
     port: 3000,
